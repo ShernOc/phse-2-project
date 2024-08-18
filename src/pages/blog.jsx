f3120d0ca3d4c1e500d7e35bde733a3f4e583be6
Silvanos Eric <sherlynea1994@hotmail.com>
2024-08-18 12:25:32 +0300

    Enhance blog and blogs pages with author images and detailed styling
    - Updated the `Blog` page to display author images using dynamically generated avatars from RoboHash.
      - Added author information, publish date, and category to the blog detail page.
      - Enhanced blog layout and styling for a cleaner presentation.
    - Updated the `Blogs` listing page to reflect consistent variable naming for blog objects.
      - Improved blog card presentation with consistent naming for blog properties.
      - Enhanced layout and styling for better readability.

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -10,11 +10,50 @@ export async function loader({ params }) {
 function Blog() {
   const { blog } = useLoaderData();
 
+  const blogWithImage = {
+    ...blog,
+    author: {
+      ...blog.author,
+      imageUrl: `https://robohash.org/${blog.id}.png?size=200x200`,
+    },
+  };
+
   return (
     <>
-      <header>
-        <h1>{blog.title}</h1>
-      </header>
+      <main className="mt-[88px] p-8 max-w-3xl mx-auto">
+        <section className="text-center">
+          {/* Heading */}
+          <h1 className="text-5xl">{blogWithImage.title}</h1>
+          {/* Author Info */}
+          <div className="relative mt-8 flex items-center justify-center gap-x-4">
+            <img
+              alt={blogWithImage.author.name}
+              src={blogWithImage.author.imageUrl}
+              className="h-10 w-10 rounded-full bg-gray-50"
+            />
+            <div className="text-sm leading-6">
+              <p className="font-semibold text-gray-900">
+                <span className="absolute inset-0" />
+                {blogWithImage.author.name}
+              </p>
+              <p className="text-gray-600">{blogWithImage.author.role}</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-4 text-xs justify-center mt-8">
+            <time dateTime={blog.datetime} className="text-gray-500">
+              {blog.date}
+            </time>
+            {/* Blog publish date and category */}
+            <span className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+              {blog.category.title}
+            </span>
+          </div>
+        </section>
+        {/* Blog post description */}
+        <section className="py-12 text-lg">
+          <p>{blog.description}</p>
+        </section>
+      </main>
     </>
   );
 }
--- src/pages/blogs.jsx
+++ src/pages/blogs.jsx
@@ -35,42 +35,42 @@ function Blogs() {
             </p>
           </div>
           <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {blogListWithRandomImage.map((post) => (
+            {blogListWithRandomImage.map((blog) => (
               <article
-                key={post.id}
+                key={blog.id}
                 className="flex max-w-xl flex-col items-start justify-between"
               >
                 <div className="flex items-center gap-x-4 text-xs">
-                  <time dateTime={post.datetime} className="text-gray-500">
-                    {post.date}
+                  <time dateTime={blog.datetime} className="text-gray-500">
+                    {blog.date}
                   </time>
                   <span className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
-                    {post.category.title}
+                    {blog.category.title}
                   </span>
                 </div>
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <Link to={`/blog/${post.id}`}>
+                    <Link to={`/blogs/${blog.id}`}>
                       <span className="absolute inset-0" />
-                      {post.title}
+                      {blog.title}
                     </Link>
                   </h3>
                   <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
-                    {post.description}
+                    {blog.description}
                   </p>
                 </div>
                 <div className="relative mt-8 flex items-center gap-x-4">
                   <img
-                    alt={post.author.name}
-                    src={post.author.imageUrl}
+                    alt={blog.author.name}
+                    src={blog.author.imageUrl}
                     className="h-10 w-10 rounded-full bg-gray-50"
                   />
                   <div className="text-sm leading-6">
                     <p className="font-semibold text-gray-900">
                       <span className="absolute inset-0" />
-                      {post.author.name}
+                      {blog.author.name}
                     </p>
-                    <p className="text-gray-600">{post.author.role}</p>
+                    <p className="text-gray-600">{blog.author.role}</p>
                   </div>
                 </div>
               </article>
