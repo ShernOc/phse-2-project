b381378602c149ca8b7af915fd20e3eb0df7722a
Silvanos Eric <sherlynea1994@hotmail.com>
2024-08-19 06:49:20 +0300

    fix(env): fallback to Node.js process.env for missing environment variables
    - Updated `getBlog.js` and `getBlogList.js` to fallback to `process.env.BLOGS_API_URL` if
      `import.meta.env.VITE_BLOGS_API_URL` is not defined
    - Modified `firebase.js` to use `process.env` as a fallback for all Firebase configuration variables
      when `import.meta.env` variables are undefined

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,23 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
-const authDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
-const projectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
-const storageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
-const appId = import.meta.env.VITE_FIREBASE_APP_ID;
-const measurementId = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
+const apiKey =
+  import.meta.env.VITE_FIREBASE_API_KEY || process.env.FIREBASE_API_KEY;
+const authDomain =
+  import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || process.env.FIREBASE_AUTH_DOMAIN;
+const projectId =
+  import.meta.env.VITE_FIREBASE_PROJECT_ID || process.env.FIREBASE_PROJECT_ID;
+const storageBucket =
+  import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ||
+  process.env.FIREBASE_STORAGE_BUCKET;
+const messagingSenderId =
+  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ||
+  process.env.FIREBASE_MESSAGING_SENDER_ID;
+const appId =
+  import.meta.env.VITE_FIREBASE_APP_ID || process.env.VITE_FIREBASE_APP_ID;
+const measurementId =
+  import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ||
+  process.env.FIREBASE_MEASUREMENT_ID;
 
 // Check for null or undefined env variables
 const envVars = {
